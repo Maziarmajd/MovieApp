a72d2bb2f696f986f30f37fa553b67c148b2e0f1
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2023-01-05 14:11:57 +0000

    create header and search box

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
-  const getMovieRequest = async() => {
-    const url = "http://www.omdbapi.com/?s=avengers&apikey=fbf48448";
+  const getMovieRequest = async(searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=fbf48448`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -18,12 +20,13 @@ function App() {
   };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
   return (
     <div className='container-fluid movie-app'>
-      <div className="row">
-        
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
         <MovieList movies={movies} />
