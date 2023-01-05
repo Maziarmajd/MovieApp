f8aa91e5236ed13b6ad5eefa55317ac2d494a70f
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2023-01-05 13:53:02 +0000

    deploy the API and fetch it

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,38 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Star Wars: Episode IV - A New Hope",
-      Year: "1977",
-      imdbID: "tt0076759",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode V - The Empire Strikes Back",
-      Year: "1980",
-      imdbID: "tt0080684",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode VI - Return of the Jedi",
-      Year: "1983",
-      imdbID: "tt0086190",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async() => {
+    const url = "http://www.omdbapi.com/?s=avengers&apikey=fbf48448";
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+    setMovies(responseJson.Search);
+  };
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
   return (
     <div className='container-fluid movie-app'>
-      <div className='row'>
+      <div className="row">
+        
+      </div>
+      <div className="row">
         <MovieList movies={movies} />
       </div>
     </div>
